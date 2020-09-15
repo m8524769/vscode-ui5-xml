77e31bd991f0b8636df2da7aff556416b823ad2b
m8524769 <m8524769@gmail.com>
2020-09-15 08:00:30 +0800

    Feat: Auto-indent

---
+++ src/extension.ts
@@ -0,0 +1,27 @@
+import { languages, ExtensionContext, LanguageConfiguration, IndentAction } from 'vscode';
+
+export function activate(context: ExtensionContext) {
+
+  const xmlConfiguration: LanguageConfiguration = {
+    indentationRules: {
+      increaseIndentPattern: /<(?!\?|[^>]*\/>)([-_\.A-Za-z0-9]+)(?=\s|>)\b[^>]*>(?!.*<\/\1>)|<!--(?!.*-->)|\{[^}"']*$/,
+      decreaseIndentPattern: /^\s*(<\/[-_\.A-Za-z0-9]+\b[^>]*>|-->|\})/
+    },
+    onEnterRules: [
+      {
+        beforeText: new RegExp(`<([_:\\w][_:\\w-.\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
+        afterText: /^<\/([_:\w][_:\w-.\d]*)\s*>/i,
+        action: { indentAction: IndentAction.IndentOutdent }
+      },
+      {
+        beforeText: new RegExp(`<(\\w[\\w\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
+        action: { indentAction: IndentAction.Indent }
+      }
+    ]
+  }
+
+  languages.setLanguageConfiguration("xml", xmlConfiguration);
+
+}
+
+export function deactivate() { }
